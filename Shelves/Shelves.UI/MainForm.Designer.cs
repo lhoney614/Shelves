f49c22bd4676e11f1aa3534ebaacd8e3c65d2e46
lhoney614 <alicewhite00@mail.ru>
2021-11-09 12:57:42 +0700

    Созданы библиотеки классов логики: параметры объекта, построитель объекта и работа с API

--- Shelves/Shelves.UI/MainForm.Designer.cs
+++ Shelves/Shelves.UI/MainForm.Designer.cs
@@ -30,26 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxC = new System.Windows.Forms.TextBox();
             this.textBoxA = new System.Windows.Forms.TextBox();
             this.textBoxB = new System.Windows.Forms.TextBox();
-            this.textBoxC = new System.Windows.Forms.TextBox();
-            this.textBoxD = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBoxE = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxD = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -73,6 +73,84 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Основные параметры полок";
             // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(196, 93);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(60, 22);
+            this.textBoxC.TabIndex = 5;
+            this.textBoxC.TextChanged += new System.EventHandler(this.textBoxC_TextChanged);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(196, 37);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(60, 22);
+            this.textBoxA.TabIndex = 4;
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(196, 65);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(60, 22);
+            this.textBoxB.TabIndex = 4;
+            this.textBoxB.TextChanged += new System.EventHandler(this.textBoxB_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(262, 96);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(121, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "от 200 до 300 мм";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(262, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "от 500 до 700 мм";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(278, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "от 15 до 20 мм";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ширина полок (C):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Длина полок (B):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Толщина досок (A):";
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.textBoxE);
@@ -90,41 +168,59 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Дополнительные параметры полок";
             // 
-            // label1
+            // textBoxE
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Толщина досок (A):";
+            this.textBoxE.Location = new System.Drawing.Point(196, 65);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(60, 22);
+            this.textBoxE.TabIndex = 5;
+            this.textBoxE.TextChanged += new System.EventHandler(this.textBoxE_TextChanged);
             // 
-            // label2
+            // label12
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 68);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 17);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Длина полок (B):";
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(81, 129);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(311, 34);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Параметр E должен быть на 50 мм меньше D.\r\nУчитывайте это при подборе высоты стен" +
+    "ок.";
             // 
-            // label3
+            // textBoxD
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 96);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Ширина полок (C):";
+            this.textBoxD.Location = new System.Drawing.Point(196, 37);
+            this.textBoxD.Name = "textBoxD";
+            this.textBoxD.Size = new System.Drawing.Size(60, 22);
+            this.textBoxD.TabIndex = 4;
+            this.textBoxD.TextChanged += new System.EventHandler(this.textBoxD_TextChanged);
             // 
-            // label4
+            // label11
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 40);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(177, 17);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Высота левой стенки (D):";
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(6, 129);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(69, 17);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "ВАЖНО!";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(262, 68);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(121, 17);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "от 100 до 150 мм";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(262, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(121, 17);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "от 150 до 200 мм";
             // 
             // label5
             // 
@@ -134,6 +230,15 @@
             this.label5.Size = new System.Drawing.Size(184, 17);
             this.label5.TabIndex = 3;
             this.label5.Text = "Высота правой стенки (E):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Высота левой стенки (D):";
             // 
             // pictureBox1
             // 
@@ -146,71 +251,6 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(215, 40);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(105, 17);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "от 15 до 20 мм";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(215, 68);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(121, 17);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "от 500 до 700 мм";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(215, 96);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(121, 17);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "от 200 до 300 мм";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(262, 40);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(121, 17);
-            this.label9.TabIndex = 4;
-            this.label9.Text = "от 150 до 200 мм";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(262, 68);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(121, 17);
-            this.label10.TabIndex = 5;
-            this.label10.Text = "от 100 до 150 мм";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label11.Location = new System.Drawing.Point(6, 129);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(69, 17);
-            this.label11.TabIndex = 6;
-            this.label11.Text = "ВАЖНО!";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(81, 129);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(311, 34);
-            this.label12.TabIndex = 7;
-            this.label12.Text = "Параметр E должен быть на 50 мм меньше D.\r\nУчитывайте это при подборе высоты стен" +
-    "ок.";
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(155, 366);
@@ -220,46 +260,6 @@
             this.button1.Text = "Построить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.buttonBuild_Click);
-            // 
-            // textBoxA
-            // 
-            this.textBoxA.Location = new System.Drawing.Point(149, 37);
-            this.textBoxA.Name = "textBoxA";
-            this.textBoxA.Size = new System.Drawing.Size(60, 22);
-            this.textBoxA.TabIndex = 4;
-            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
-            // 
-            // textBoxB
-            // 
-            this.textBoxB.Location = new System.Drawing.Point(149, 65);
-            this.textBoxB.Name = "textBoxB";
-            this.textBoxB.Size = new System.Drawing.Size(60, 22);
-            this.textBoxB.TabIndex = 4;
-            this.textBoxB.TextChanged += new System.EventHandler(this.textBoxB_TextChanged);
-            // 
-            // textBoxC
-            // 
-            this.textBoxC.Location = new System.Drawing.Point(149, 93);
-            this.textBoxC.Name = "textBoxC";
-            this.textBoxC.Size = new System.Drawing.Size(60, 22);
-            this.textBoxC.TabIndex = 5;
-            this.textBoxC.TextChanged += new System.EventHandler(this.textBoxC_TextChanged);
-            // 
-            // textBoxD
-            // 
-            this.textBoxD.Location = new System.Drawing.Point(196, 37);
-            this.textBoxD.Name = "textBoxD";
-            this.textBoxD.Size = new System.Drawing.Size(60, 22);
-            this.textBoxD.TabIndex = 4;
-            this.textBoxD.TextChanged += new System.EventHandler(this.textBoxD_TextChanged);
-            // 
-            // textBoxE
-            // 
-            this.textBoxE.Location = new System.Drawing.Point(196, 65);
-            this.textBoxE.Name = "textBoxE";
-            this.textBoxE.Size = new System.Drawing.Size(60, 22);
-            this.textBoxE.TabIndex = 5;
-            this.textBoxE.TextChanged += new System.EventHandler(this.textBoxE_TextChanged);
             // 
             // MainForm
             // 
